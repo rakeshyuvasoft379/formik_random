009df42dee33eb9fe3c611dce94b3822432ab9df
rakesh.kumawat <rakeshyuvasoft379@gmail.com>
2023-01-03 00:31:14 +0530

    first commit on feature

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -23,7 +23,7 @@ export const Form=()=>{
             value={formik.values.email}
             />
             <button type="submit">submit</button>
-            <button>demo</button>
+            <button>demo2</button>
         </form>
     )
 }
